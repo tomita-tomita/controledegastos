505c1d34d9c744c9ebea6389ba847fd900205d04
Eduardo Tomita <duh.tomita@hotmail.com>
2017-07-05 22:45:23 -0300

    Finalizando backend

---
+++ backend/api/cicloSumario/cicloSumarioService.js
@@ -0,0 +1,21 @@
+const _ = require('lodash')
+const Ciclo = require('../ciclo/ciclo')
+
+
+function getSummary(req, res){
+    Ciclo.aggregate({
+        $project: {credito: {$sum: "$creditos.value"}, debito: {$sum: "$debitos.value"}}
+    },{
+        $group: {_id: null, credito: {$sum: "$credito"}, debito: {$sum: "$debito"}}
+    },{
+        $project: {_id: 0, credito: 1, debito: 1}
+    }, function(error, result){
+        if(error){
+            res.status(500).json({errors: [error]})
+        }else{
+            res.json(_.defaults(result[0], {credito: 0, debito: 0}))
+        }
+    })
+}
+
+module.exports = { getSummary }
--- backend/config/rotas.js
+++ backend/config/rotas.js
@@ -2,9 +2,12 @@ const express = require('express')
 
 module.exports = function(server){
 
- const rota = express.Router()
- server.use('/api', rota)
+    const rota = express.Router()
+    server.use('/api', rota)
 
-const cicloService = require('../api/ciclo/cicloService')
-cicloService.register(rota, '/ciclos')
+    const cicloService = require('../api/ciclo/cicloService')
+    cicloService.register(rota, '/ciclos')
+
+    const cicloSumarioService = require('../api/cicloSumario/cicloSumarioService')
+    rota.route('/cicloSummary').get(cicloSumarioService.getSummary)
 }
